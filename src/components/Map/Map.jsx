9efc15ddc3b4a8c729d5048f10efed1887099249
othea32 <othea32@gmail.com>
2023-08-02 15:44:11 -0700

    updated Map.js and styles. Added my 1st api Google Maps

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+import useStyles from './styles.js'
 
 const Map = () => {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px)')
+
+  const coordinates = { lat: 0, lng: 0 }
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDrlOfv_JUjmPYHTTd6Bf_i07EE86qNEUY' }}
+        defaultCenter={{ coordinates }}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
